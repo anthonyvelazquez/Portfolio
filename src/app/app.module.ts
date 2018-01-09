5a23e4f300e89a2de4b613bcdb84b9df436c278d
System on Grid <systemongrid@Systems-iMac.local>
2018-01-09 15:29:55 -0500

    Intro Transition Done

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,19 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 
 import { AppComponent } from './app.component';
+import { IntroPageComponent } from './intro-page/intro-page.component';
+import { AboutPageComponent } from './about-page/about-page.component';
+import { ProjectPageComponent } from './project-page/project-page.component';
+import { ContactPageComponent } from './contact-page/contact-page.component';
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    IntroPageComponent,
+    AboutPageComponent,
+    ProjectPageComponent,
+    ContactPageComponent
   ],
   imports: [
     BrowserModule,
