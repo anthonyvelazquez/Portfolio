4e7b0e4eea2f46f158b27bf7f584be76a6b833b3
System on Grid <systemongrid@Systems-iMac.local>
2018-01-10 15:00:27 -0500

    Update

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,27 +1,9 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
-import { IntroPageComponent } from './intro-page/intro-page.component';
-import { ProjectPageComponent } from './project-page/project-page.component';
-import { AboutPageComponent } from './about-page/about-page.component';
-import { ContactPageComponent } from './contact-page/contact-page.component';
+
 
 const routes: Routes = [
-  {
-    path: '',
-    component: IntroPageComponent,
-  },
-  {
-    path: 'projects',
-    component: ProjectPageComponent,
-  },
-  {
-    path: 'about',
-    component: AboutPageComponent,
-  },
-  {
-    path: 'contact',
-    component: ContactPageComponent,
-  },
+
 ];
 
 @NgModule({
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,19 +4,11 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 
 import { AppComponent } from './app.component';
-import { IntroPageComponent } from './intro-page/intro-page.component';
-import { AboutPageComponent } from './about-page/about-page.component';
-import { ProjectPageComponent } from './project-page/project-page.component';
-import { ContactPageComponent } from './contact-page/contact-page.component';
 
 
 @NgModule({
   declarations: [
     AppComponent,
-    IntroPageComponent,
-    AboutPageComponent,
-    ProjectPageComponent,
-    ContactPageComponent
   ],
   imports: [
     BrowserModule,
